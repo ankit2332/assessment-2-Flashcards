7a1079df5674b0e9f4769de8ce12bd6ff5349ae9
HP <HP@DESKTOP-0IMS8QA>
2021-03-16 15:01:41 +0700

    Add project files.

---
+++ assessment 2 Flashcards/Card.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assessment_2_Flashcards
+{
+    class Card
+    {
+        private string word;
+        private string def;
+        private bool flipped;
+
+        public Card(string word, string def)
+        {
+            this.word = word;
+            this.def = def;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Gets the word on the card.</returns>
+        public string getword()
+        {
+            return word;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Gets the definision of the word.</returns>
+        public string getdef()
+        {
+            return def;
+        }
+
+        /// <summary>
+        /// checks if the card has been flipped yet.
+        /// </summary>
+        /// <returns>Prints True or false. if it have been flipped then it will print true, If not then false.</returns>
+        public bool isflipped()
+        {
+            return flipped;
+        }
+
+        /// <summary>
+        /// This flips the card around.
+        /// </summary>
+        public void flip()
+        {
+            if(flipped == false)
+            {
+                flipped = true;
+            }
+            else
+            {
+                flipped = false;
+            }
+        }
+    }
+}
---
+++ assessment 2 Flashcards/Deck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assessment_2_Flashcards
+{
+    class Deck
+    {
+        Card[] cards;
+        private string filename;
+        private int topcard = 0;
+
+        public Deck(string filename)
+        {
+            this.filename = filename;
+            cards = new Card[4];
+        }
+
+        public void loadfile()
+        {
+            int count = 0;
+            string line;
+            StreamReader fileReader = new StreamReader(filename);
+            while((line = fileReader.ReadLine()) != null)
+            {
+                string[] cell = line.Split(',');
+                string word = cell[0];
+                string def = cell[1];
+                cards[count] = new Card(word, def);
+                count++;
+            }
+        }
+
+    }
+}
